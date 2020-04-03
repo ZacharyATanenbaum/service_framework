2c458bf570641cfa94b96decf573dffa444cbcc0
ZacharyATanenbaum <ZachTanenbaum@gmail.com>
2020-04-03 00:23:52 -0400

    Added unit test for loading json file from relative path

--- tests/unit_tests/utils_unit_test.py
+++ tests/unit_tests/utils_unit_test.py
@@ -3,6 +3,7 @@
 from service_framework.utils import utils
 
 MODULE_TO_IMPORT_PATH = './tests/unit_tests/data/utils_unit_test/module_to_import.py'
+JSON_FILE_TO_LOAD = './tests/unit_tests/data/utils_unit_test/file_to_load.json'
 
 
 def test_utils__snake_case_to_capital_case__valid_case():
@@ -64,3 +65,19 @@ def test_utils__import_python_file_from_module__valid_case():
     """
     module_path = 'service_framework.connections.in.replyer'
     _ = utils.import_python_file_from_module(module_path)
+
+
+def test_utils__get_json_from_rel_path__no_rel_path_case():
+    """
+    Make sure that the file won't break if you don't provide a rel path.
+    """
+    response = utils.get_json_from_rel_path(None)
+    assert response == {}
+
+
+def test_utils__get_json_from_rel_path__properly_import_json_file_case():
+    """
+    Make sure the function can actually do its job.
+    """
+    loaded_file = utils.get_json_from_rel_path(JSON_FILE_TO_LOAD)
+    assert loaded_file['SEE_IT_WORKS'] == 'SUCCESS'
